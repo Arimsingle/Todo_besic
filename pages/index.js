b4cd535ec9f1b7e5c948faba8fbf2d8f8690f5b7
Arima <55054011+Arimsingle@users.noreply.github.com>
2020-02-18 18:06:06 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ const Index = () => {
         { Name: 'Arima' },
     ])
     const [subtext, setSubtext] = useState('')
-    const Enter =(e)=>{
+    const Enter_ =(e)=>{
         e.preventDefault();
         setText([...text,{Name:subtext}])
     }
     return (
         <div>
             <div>
-                <form onSubmit={Enter}>
+                <form onSubmit={Enter_}>
                     <input type="text" name="name"  onChange={(e)=>setSubtext(e.target.value)} />
                 </form>
             </div>
